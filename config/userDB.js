0a2774920b7434c1223357a610c20874c97855f6
Team Phoenix <71386759+ResidentApp@users.noreply.github.com>
2020-09-20 00:42:13 +0530

    Update userDB.js

--- config/userDB.js
+++ config/userDB.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongoURI');
+const db = process.env.URI1;
 
 const connectUserDB = async () => {
   try {
