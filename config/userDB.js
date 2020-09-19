a3a8d234146a30dbf20c228efca77b0dc2a29402
Shubham Deodhar <deodhar2000@gmail.com>
2020-09-19 06:30:32 +0530

    feat(mvcPattern):boilerplate

---
+++ config/userDB.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = process.env.mongoURI1;
+
+const connectUserDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
+    console.log('User database connected');
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectUserDB;
