3efc73413136d6a7a9d2d6a4450702035c2d280d
Team Phoenix <71386759+ResidentApp@users.noreply.github.com>
2020-09-20 00:51:21 +0530

    Update testRoute.js

--- Routes/PostRoutes/testRoute.js
+++ Routes/PostRoutes/testRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../../Middleware/Auth');
+const auth = require('../../Middleware/auth');
 const Users = require('../../Modals/user');
 
 //test route
