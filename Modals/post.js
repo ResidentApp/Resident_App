e64f6a438ec5f6952c5111394a84dc5d16a6d699
Saumitr Joshi <saumitra1joshi@gmail.com>
2020-09-19 11:25:53 +0530

    fix(Schema ):complete

--- Modals/post.js
+++ Modals/post.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const locationSchema = new Schema({
-  longitude: {
-    type: String,
-    required: true,
-  },
-  latitude: {
-    type: String,
-    required: true,
-  },
+    type: {
+      type: String,
+      default: "Point"
+    },
+    coordinates: {
+      type: [Number],
+      index: "2dsphere"
+    }
 });
 
 const PostSchema = new Schema(
