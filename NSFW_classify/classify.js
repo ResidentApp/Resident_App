a60b1cbc737270f41297357d16cfe5c89b4f4f25
Pranshul <dobriyalpranshul@gmail.com>
2020-09-20 01:02:04 +0530

    Fixed errors

--- NSFW_classify/classify.js
+++ NSFW_classify/classify.js
@@ -1,66 +1,57 @@
-function classify_comment(comment){
-        const {google} = require('googleapis');
+async function classify_comment(comment) {
+  const { google } = require("googleapis");
+  const API_KEY = "AIzaSyDvG3vlovfaGB-ZxN27LHIIFbuzIRr6sz0";
+  const DISCOVERY_URL =
+    "https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1";
+  const client = await google.discoverAPI(DISCOVERY_URL);
+  const resp = await client.comments.analyze({
+    key: API_KEY,
+    resource: {
+      comment: {
+        text: "Trump is an idiot",
+      },
+      requestedAttributes: {
+        TOXICITY: {},
+      },
+    },
+    requestedAttributes: {
+      TOXICITY: {},
+    },
+  });
 
-        API_KEY = 'AIzaSyDvG3vlovfaGB-ZxN27LHIIFbuzIRr6sz0';
-        DISCOVERY_URL =
-            'https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1';
-
-        google.discoverAPI(DISCOVERY_URL)
-         .then(client => {
-                const analyzeRequest = {
-                comment: {
-                text: comment,
-                },
-                requestedAttributes: {
-                TOXICITY: {},
-                },
-            };
-
-        client.comments.analyze(
-          {
-            key: API_KEY,
-            resource: analyzeRequest,
-          },
-          (err, response) => {
-            if (err) throw err;
-            if(response.data.attributeScores.TOXICITY.spanScores[0].score.value>0.45){
-                return true
-            }
-            else {
-                return false
-            }
-          });
-     })
-    .catch(err => {
-      throw err;
-    });
-
+  if (resp.data.attributeScores.TOXICITY.spanScores[0].score.value > 0.45) {
+    return true;
+  } else {
+    return false;
+  }
 }
+async function classify_image(img_link) {
+  const axios = require("axios").default; // install request module by - 'npm install request'
+  const querystring = require("querystring");
 
-function classify_image(img_link)
-{
-    var request = require('request');   // install request module by - 'npm install request'
-    var querystring = require('querystring')
+  const form_data = {
+    modelId: "e4745b0e-70fb-447e-8870-61566137c8ab",
+    urls: img_link,
+  };
 
-    const form_data = {
-        'modelId' : 'e4745b0e-70fb-447e-8870-61566137c8ab',
-     'urls': img_link
-    }
+  const options = {
+    headers: {
+      Authorization:
+        "Basic " +
+        Buffer.from("-haYmFRR1siZGxd6GqF0fB3frdMLXqV5" + ":").toString(
+          "base64"
+        ),
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  };
 
-    const options = {
-        url : 'https://app.nanonets.com/api/v2/ImageCategorization/LabelUrls/',
-        body: querystring.stringify(form_data),
-        headers: {
-            'Authorization' : 'Basic ' + Buffer.from('-haYmFRR1siZGxd6GqF0fB3frdMLXqV5' + ':').toString('base64'),
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    }
-    request.post(options, function(err, httpResponse, body) {
-        body = JSON.parse(body)
-        if(body.result[0].prediction[0].probability<0.6)
-            return true
-        else
-            return false
-    });
+  const resp = await axios.post(
+    "https://app.nanonets.com/api/v2/ImageCategorization/LabelUrls/",
+    querystring.stringify(form_data),
+    options
+  );
+  const body = resp.data;
+  if (body.result[0].prediction[0].probability < 0.6) return true;
+  return false;
 }
 module.exports = {classify_comment, classify_image}
--- NSFW_classify/test.js
+++ NSFW_classify/test.js
@@ -1,12 +1,16 @@
 const nlp = require('./classify')
-const a = nlp.classify_comment("Trump is an idiot")
-console.log(a)
-if( a == true){
-    console.log("Toxic")
-}
-else if(a == false) {
-    console.log("Not Toxic")
-}
-const link = 'https://miro.medium.com/max/875/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg'
 
-console.log(nlp.classify_image(link))
+async function main() {
+  const a = await nlp.classify_comment("Trump is an idiot")
+  if( a == true){
+      console.log("Toxic")
+   }
+  else if(a == false) {
+    console.log("Not Toxic")
+  }
+   const link = 'https://miro.medium.com/max/875/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg'
+   b =  await nlp.classify_image(link)
+   console.log(b)
+}
+
+main()
